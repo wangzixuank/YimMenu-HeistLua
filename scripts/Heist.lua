f6233ed7896c8098e5ab8781f72d3265621b2bca
nie <996347917@qq.com>
2023-07-10 11:19:49 +0800

    v1.72修正tab问题

--- scripts/Heist.lua
+++ scripts/Heist.lua
@@ -1,7 +1,7 @@
--- v1.71 -- 感谢Alice和sch的指导
+-- v1.72 -- 感谢Alice和sch的指导
 local tabName = "Heist"
--- local myTab = tab:add_tab(tabName)
-local myTab = gui.get_tab(tabName)
+local myTab = gui:add_tab(tabName)
+-- local myTab = gui.get_tab(tabName)
 
 -- package.path = os.getenv("UserProfile").."/AppData/Roaming/YimMenu/scripts/?.lua"
 require("Alice-lib/lib")
